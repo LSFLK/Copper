10e6a48263eb1b10ba3192e2d2218159a3e3430e
anuudb <anu.udb@gmail.com>
2018-11-20 11:26:40 +0530

    Reset to previous stage

---
+++ copper-server/docker/webmail/horde-webmail/proxy_client_ip.php
@@ -0,0 +1,12 @@
+<?php
+
+if( isset( $_SERVER[ 'HTTP_X_FORWARDED_FOR' ] ) ) {
+    $_SERVER[ 'REMOTE_ADDR' ] = $_SERVER[ 'HTTP_X_FORWARDED_FOR' ];
+}
+
+if ( isset($_SERVER[ 'HTTP_X_FORWARDED_PROTO' ])) {
+    $_SERVER['HTTPS']='on';
+    #$_SERVER['HTTP']='on';
+}
+
+?>
