e62fa36a5223f9b8ec2a00c6d3a1383e46a5de03
tharangar <tharanga.rajapaksha@gmail.com>
2019-01-30 13:43:23 +0530

    Bug fixing in deploymrnt sh.

--- copper-server/kubernetes/deployment/deploy.sh
+++ copper-server/kubernetes/deployment/deploy.sh
@@ -191,31 +191,31 @@ sleep 1s
 cd ..
 
 #Buld the kubernetes pod
-Kubectl create -f copperclient/webmail.yaml 
+Kubectl create -f copperclient/webmail.yaml 2> /dev/null || true
 echoGreenBold 'Docker webclient service created...'
 #Prometheus implementation
 # Creating a roll has the access for clusters and bind the cluster roll.
-kubectl create -f prometheus-alert/clusterRole.yaml
+kubectl create -f prometheus-alert/clusterRole.yaml 2> /dev/null || true
 echoGreenBold 'Role creation and Role binding...'
 # Create the config map to keep configuration data of prometheus
-kubectl create -f prometheus-alert/config-map.yaml -n monitoring
+kubectl create -f prometheus-alert/config-map.yaml -n monitoring 2> /dev/null || true
 echoGreenBold 'Prometheus configuration created...'
 # Deploy prometheus pods 
-kubectl create  -f prometheus-alert/prometheus-deployment.yaml --namespace=monitoring
+kubectl create  -f prometheus-alert/prometheus-deployment.yaml --namespace=monitoring 2> /dev/null || true
 
 # Create the service to access prometheus 
-kubectl create -f prometheus-alert/prometheus-service.yaml --namespace=monitoring
+kubectl create -f prometheus-alert/prometheus-service.yaml --namespace=monitoring 2> /dev/null || true
 echoGreenBold 'Prometheus service created...'
 # Alert manager implementation
 # Creating the configuration 
-kubectl create -f prometheus-alert/AlertManagerConfigmap.yaml
+kubectl create -f prometheus-alert/AlertManagerConfigmap.yaml 2> /dev/null || true
 #
-kubectl create -f prometheus-alert/AlertTemplateConfigMap.yaml
+kubectl create -f prometheus-alert/AlertTemplateConfigMap.yaml 2> /dev/null || true
 echoGreenBold 'Alert Manager congiguration created..'
 #
-kubectl create -f prometheus-alert/Deployment.yaml
+kubectl create -f prometheus-alert/Deployment.yaml 2> /dev/null || true
 #
-kubectl create -f prometheus-alert/Service.yaml
+kubectl create -f prometheus-alert/Service.yaml 2> /dev/null || true
 echoGreenBold 'Alert Manager created...'
 
 
