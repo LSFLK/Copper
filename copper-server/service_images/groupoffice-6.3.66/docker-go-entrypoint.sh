46c46bcc2126d0b8cbfbc5616e3646541eb50db0
tharangar <tharanga.rajapaksha@gmail.com>
2019-06-07 17:01:55 +0530

    Ingress

--- copper-server/service_images/groupoffice-6.3.66/docker-go-entrypoint.sh
+++ copper-server/service_images/groupoffice-6.3.66/docker-go-entrypoint.sh
@@ -10,5 +10,16 @@ sed -i 's/{dbName}/'${MYSQL_DATABASE}'/' /etc/groupoffice/config.php
 sed -i 's/{dbUser}/'${MYSQL_USER}'/' /etc/groupoffice/config.php
 sed -i 's/{dbPass}/'${MYSQL_PASSWORD}'/' /etc/groupoffice/config.php
 
+# >>> https convertion works added by me >>>>
+
+#mkdir /etc/apache2/ssl
+
+#cd /etc/apache2/ssl
+
+#openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.csr
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 #call original entry point
 docker-php-entrypoint "$@"
